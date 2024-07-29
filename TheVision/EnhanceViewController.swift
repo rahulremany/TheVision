7029047025c929a116dd1223d25a85d13bc56843
JayZeeXx <rahulremany@gmail.com>
2024-07-29 15:25:08 -0700

    Launch Screen completed as well as comments and app icon

--- TheVision/EnhanceViewController.swift
+++ TheVision/EnhanceViewController.swift
@@ -4,6 +4,8 @@ import Vision
 import Photos
 
 class EnhanceViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // Outlets for all of the views on the page
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var doItForMeButton: UIButton!
     @IBOutlet weak var uiLabel: UILabel!
@@ -18,8 +20,10 @@ class EnhanceViewController: UIViewController, UIImagePickerControllerDelegate, 
     @IBOutlet weak var saveLabel: UILabel!
     @IBOutlet weak var revertButton: UIButton!
     
+    // Global variable for our original image in the case of revertion
     var originalImage: UIImage?
 
+    // Initial view function that hides everything except the label at the top and the select image label
     override func viewDidLoad() {
         super.viewDidLoad()
         diffImageBtn.isHidden = true
@@ -32,14 +36,18 @@ class EnhanceViewController: UIViewController, UIImagePickerControllerDelegate, 
         contrastLabel.isHidden = true
         doItForMeButton.isHidden = true
         saveLabel.isHidden = true
+        revertButton.isHidden = true
         
         // Set default slider values
         brightnessSlider.value = 0
         contrastSlider.value = 1
         saturationSlider.value = 1
-        revertButton.isHidden = true // Initially hide the revert button
     }
 
+    /**
+     Following functions serve as the action handlers for all the buttons on the screen
+     They call external functions in the app
+     **/
     @IBAction func selectPictureTapped(_ sender: UIButton) {
         presentImagePicker()
     }
@@ -68,29 +76,37 @@ class EnhanceViewController: UIViewController, UIImagePickerControllerDelegate, 
         revertToOriginalImage()
     }
 
-        
+    // Code to generate any alert boxes required
     func showAlert(title: String, message: String) {
         let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
         present(ac, animated: true)
     }
     
+    //
     func applyFilters() {
+        // Ensure there's an original image to work with
         guard let originalImage = originalImage else { return }
+        
+        // Get the CGImage from the original UIIMage
         guard let cgImage = originalImage.cgImage else { return }
         
+        // Create a CIImage from the CGImage considering the orientation
         let inputImage = CIImage(cgImage: cgImage, options: [CIImageOption.applyOrientationProperty: true])
         
-        let brightness = brightnessSlider.value // -1.0 to 1.0
-        let contrast = contrastSlider.value * 2.0 // 0.0 to 4.0 (Slider 0.0 to 2.0)
-        let saturation = saturationSlider.value * 2.0 // 0.0 to 2.0
+        // Get the values for the sliders
+        let brightness = brightnessSlider.value // range: -1.0 to 1.0
+        let contrast = contrastSlider.value * 2.0 // range: 0.0 to 4.0 (Slider 0.0 to 2.0)
+        let saturation = saturationSlider.value * 2.0 // range: 0.0 to 2.0
         
+        // Create the CIFilter that controls the color controls
         let filter = CIFilter(name: "CIColorControls")
         filter?.setValue(inputImage, forKey: kCIInputImageKey)
         filter?.setValue(brightness, forKey: kCIInputBrightnessKey)
         filter?.setValue(contrast, forKey: kCIInputContrastKey)
         filter?.setValue(saturation, forKey: kCIInputSaturationKey)
         
+        // Create a CIContext to render the filtered image
         let context = CIContext(options: nil)
         if let outputImage = filter?.outputImage,
            let cgOutputImage = context.createCGImage(outputImage, from: outputImage.extent) {
@@ -98,16 +114,21 @@ class EnhanceViewController: UIViewController, UIImagePickerControllerDelegate, 
         }
     }
     
+    // Function to handle the "Do it for me" button that uses the AI model to enhance the image using Super Resolution
     func enhanceImageAutomatically() {
+        // Ensure we have an image
         guard let image = imageView.image else { return }
 
+        // Create a pixel buffer to write the new image to
         guard let pixelBuffer = image.toCVPixelBuffer() else { return }
 
+        // Ensure we can load the model
         guard let model = try? VNCoreMLModel(for: aesrgan(configuration: MLModelConfiguration()).model) else {
             print("Failed to load model")
             return
         }
 
+        // Create a request to the AESRGAN model and get the result as an output buffer and then push it into an image
         let request = VNCoreMLRequest(model: model) { [weak self] (request, error) in
             if let results = request.results as? [VNPixelBufferObservation], let outputBuffer = results.first?.pixelBuffer {
                 let outputImage = UIImage(pixelBuffer: outputBuffer)
@@ -117,10 +138,12 @@ class EnhanceViewController: UIViewController, UIImagePickerControllerDelegate, 
             }
         }
 
+        // Define a handler for the request and let it perform the request
         let handler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:])
         try? handler.perform([request])
     }
 
+    // Function to open the image selector
     func presentImagePicker() {
         let imagePickerController = UIImagePickerController()
         imagePickerController.delegate = self
@@ -128,6 +151,7 @@ class EnhanceViewController: UIViewController, UIImagePickerControllerDelegate, 
         present(imagePickerController, animated: true, completion: nil)
     }
 
+    // Controller that handles the state of the screen once an image is selected
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let selectedImage = info[.originalImage] as? UIImage {
             originalImage = selectedImage // Store the original image
@@ -150,10 +174,12 @@ class EnhanceViewController: UIViewController, UIImagePickerControllerDelegate, 
         dismiss(animated: true, completion: nil)
     }
 
+    // Function to close the picker once an image is selected
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
     }
     
+    // Function to revert back to the original image
     func revertToOriginalImage() {
         imageView.image = originalImage
         brightnessSlider.value = 0
@@ -164,6 +190,7 @@ class EnhanceViewController: UIViewController, UIImagePickerControllerDelegate, 
 
 /**
  Extension of UIImage to add function to convert Image to CVPixelBuffer, which is what the model needs to work with
+ Got off online
  **/
 extension UIImage {
     func toCVPixelBuffer() -> CVPixelBuffer? {
